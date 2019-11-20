ae9e08a9a27a8e07c37ede60830d655ea7115f4c
Jen Merritt <jenjomez@gmail.com>
2019-11-20 16:15:51 +0000

    exercise c q2-3 completed

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -60,7 +60,12 @@ users["Jonathan"][:twitter]
 
 # 2. Get Erik's hometown
 
+users["Erik"][:home_town]
+
 # 3. Get the array of Erik's lottery numbers
+
+users["Erik"][:lottery_numbers]
+
 # 4. Get the species of Avril's pet Monty
 # 5. Get the smallest of Erik's lottery numbers
 # 6. Return an array of Avril's lottery numbers that are even
