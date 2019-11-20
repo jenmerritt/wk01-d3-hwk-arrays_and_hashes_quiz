ed58b8004340e0557012a0b9f0f14a6fffae6fc0
Jen Merritt <jenjomez@gmail.com>
2019-11-20 21:23:05 +0000

    exercise a revision q8

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -15,10 +15,10 @@ stops.delete_at(2)
 #7. Print the number of stops there are in the array?
 p stops.count
 #8. Show as many ways as you can to return "Falkirk High" from the array?
-p stops[2]
-p stops.at(2)
-p stops.slice(2)
-p stops.fetch(2)
+stops[2]
+stops.at(2)
+stops.slice(2)
+stops.fetch(2)
 #9. Reverse the positions of the stops in the array
 p stops.reverse
 #10 Print out all the stops using a for loop
